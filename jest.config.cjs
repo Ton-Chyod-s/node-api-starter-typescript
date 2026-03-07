2c51f6d397329d92aa3866139f3f0d98a4f046f4
Ton-Chyod-s <klayton.dias@hotmail.com>
2026-03-07 19:26:13 -0400

    feat: refatorar configuração do Jest para modularizar paths e melhorar a estrutura do arquivo

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,23 +2,41 @@
 const { pathsToModuleNameMapper } = require('ts-jest');
 const { compilerOptions } = require('./tsconfig.json');
 
-module.exports = {
+const tsPaths = pathsToModuleNameMapper(compilerOptions.paths || {}, {
+  prefix: '<rootDir>/',
+});
+
+const baseConfig = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testTimeout: 30000,
-  roots: ['<rootDir>/tests'],
-  moduleFileExtensions: ['ts', 'js', 'json'],
-
   setupFiles: ['<rootDir>/tests/setup/test-env.js'],
-
   transform: {
     '^.+\\.tsx?$': ['ts-jest', { diagnostics: false }],
   },
+};
 
-  moduleNameMapper: {
-    '^@prisma/client$': '<rootDir>/tests/setup/prisma-client-mock.ts',
-    ...pathsToModuleNameMapper(compilerOptions.paths || {}, {
-      prefix: '<rootDir>/',
-    }),
-  },
+module.exports = {
+  projects: [
+    {
+      ...baseConfig,
+      displayName: 'unit',
+      roots: ['<rootDir>/tests'],
+      testPathIgnorePatterns: ['<rootDir>/tests/integration'],
+      moduleNameMapper: {
+        '^@prisma/client$': '<rootDir>/tests/setup/prisma-client-mock.ts',
+        ...tsPaths,
+      },
+    },
+
+    {
+      ...baseConfig,
+      displayName: 'integration',
+      testTimeout: 60000,
+      testMatch: ['<rootDir>/tests/integration/**/*.spec.ts'],
+      moduleNameMapper: {
+        ...tsPaths,
+      },
+    },
+  ],
 };
